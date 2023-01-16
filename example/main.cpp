2dfc98d69d059c63e445c901947b227936dda2c9
Erik Meinders <erik@easytocloud.com>
2023-01-16 15:11:55 +0100

    Finalized some startup code and cleanup of example

--- example/main.cpp
+++ example/main.cpp
@@ -5,10 +5,13 @@
 
 WebServer httpServer;
 
+DECLARE_TIMER( YourCodeShoutOut, 5 );
+
 void knxapp_setup() {
+  
   // put your setup code here, to run once:
 
-  Debugln(" >> ERIX SETUP << ");
+  Debugln(" >> YOUR CODE SETUP << ");
 
   apiInit(&httpServer);
 }
@@ -19,7 +22,8 @@ void knxapp_loop() {
 
   httpServer.handleClient();
 
-  Debugln(" >> ERIX LOOP << ");
+  if (DUE(YourCodeShoutOut ))
+    Debugln(" >> YOUR CODE LOOP << ");
 }
 
 char * knxapp_hostname()
