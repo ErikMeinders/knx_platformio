3193eff879ee65a94a48d37e0de8db7d27e2802e
Erik Meinders <erik@easytocloud.com>
2023-01-16 15:58:57 +0100

    [Fix] Improved response during startup

--- lib/knxp_platformio.cpp
+++ lib/knxp_platformio.cpp
@@ -8,11 +8,14 @@ void knxapp_setup();
 void knxapp_loop();
 char *knxapp_hostname();
 
-//
-
+/**
+ * @brief Loop until KNX is configured or timeout is reached 
+ * 
+ */
 void getKNXconfigured()
 {
     uint16_t ia = knx.individualAddress();
+    time_t configstart = millis();
     
     Serial.printf("Individual Address: %d.%d.%d\n", ia >> 12, (ia >> 8) & 0x0F, ia & 0xFF);
     Serial.printf("Configured: %s\n", knx.configured() ? "true" : "false");
@@ -21,12 +24,12 @@ void getKNXconfigured()
 
     // if(knx.individualAddress() == 0xFFFF) 
     knx.progMode(true);
-
     knx.loop();
+
     while( !knx.configured())
     {
         knx.loop();
-        if (millis() > PROGMODE_TIMEOUT && knx.progMode() ){
+        if (millis() > PROGMODE_TIMEOUT + configstart && knx.progMode() ){
             knx.progMode(false);
            
             Serial.printf("Timeout, exiting prog mode\n");
@@ -34,7 +37,8 @@ void getKNXconfigured()
         }
         delay(5);
     }
-        
+    knx.loop();
+
     ia = knx.individualAddress();
     
     Serial.printf("Individual Address: %d.%d.%d\n", ia >> 12, (ia >> 8) & 0x0F, ia & 0xFF);
@@ -67,21 +71,27 @@ void setup()
     knxapp_report(step++, "Starting KNX configuration");
     getKNXconfigured();
     knx.start();
-    
+    knx.loop();
+
     knxapp_report(step++, "Starting MDNS");
     startMDNS(knxapp_hostname());
+    knx.loop();
 
     knxapp_report(step++, "Starting OTA");
     otaInit();
+    knx.loop();
 
     knxapp_report(step++, "Starting Webserver");
     httpServer.begin();
-    
+    knx.loop();
+
     knxapp_report(step++, "Starting Telnet");
     startTelnet();
+    knx.loop();
 
     knxapp_report(step++, "Starting KNX Application");
     knxapp_setup();
+    knx.loop();
 
     resetUptime();
     Debugf("-- [%s] Startup completed in %d ms.\n\n", timeNowString(), millis());
@@ -92,7 +102,7 @@ void setup()
  * Do not continue with application loop until KNX is ready and not in programming mode
  * 
  */
-DECLARE_TIMERms(httpHandle, 500);
+DECLARE_TIMERms(httpHandle, 250);
 void loop()
 {
     knx.loop();
@@ -102,9 +112,14 @@ void loop()
     if(!knx.configured() || knx.progMode()) return;
 
     otaLoop();
+    knx.loop();
+
     handleHeartbeat();
+    knx.loop();
 
     httpServer.handleClient();
+    knx.loop();
 
     timeThis ( knxapp_loop() );
+    knx.loop();
 }
