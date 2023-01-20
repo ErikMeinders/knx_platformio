0d6a529121fe91e4e1bda593acbc57e36807b9e3
Erik Meinders <erik@easytocloud.com>
2023-01-20 08:19:31 +0100

    Added dump statements

--- example/api.h
+++
@@ -1,6 +0,0 @@
-#include <Arduino.h>
-#include <ArduinoJson.h>
-
-#include "knx_platformio.h"
-
-void apiInit(WebServer *_srv);
--- lib/knxp_platformio.cpp
+++ lib/knxp_platformio.cpp
@@ -8,6 +8,9 @@ void knxapp_setup();
 void knxapp_loop();
 char *knxapp_hostname();
 
+void dump_status();
+void dumpParameters();
+void dumpGroupAddresses();
 /**
  * @brief Loop until KNX is configured or timeout is reached 
  * 
@@ -45,6 +48,57 @@ void getKNXconfigured()
     
     Serial.printf("Individual Address: %d.%d.%d\n", ia >> 12, (ia >> 8) & 0x0F, ia & 0xFF);
     Serial.printf("Configured: %s\n", knx.configured() ? "true" : "false");
+    if( knx.configured()) {
+        dumpParameters();
+        dumpGroupAddresses();
+    }
+}
+
+void dumpGroupAddresses()
+{
+    GroupObject *go;
+    
+    Serial.printf("Group Addresses:\n");
+    for (int i = 1; i < 2; i++)
+    {
+        Serial.print("GO %02d\n",i);
+        go = knx.groupObject(i);
+
+        Serial.printf(Flags: ");
+        Serial.printf(go->responseUpdateEnable() ? "U" : "-");
+        Serial.printf(go->transmitEnable() ? "T" : "-");
+        Serial.printf(go->readEnable() ? "R" : "-");
+        Serial.printf(go->writeEnable() ? "W" : "-");
+        Serial.printf(go->valueReadOnInit() ? "I" : "-");
+        Serial.printf("valueSize: %d ",go->valueSize());
+        Serial.printf("value: %0*X ", go->valueSize(), go->value());
+        Serial.printf("DPT: %d ",go->dataPointType());
+        Serial.printf("Com Flag: %d \n",go->comFlag());
+
+    }
+}
+
+void dumpParameters()
+{
+    Serial.printf("Parameters:\n");
+    for (int i = 0; i < 2; i++)
+    {
+        Serial.print("P%02d: %2X\n",i,knx.parameterByte(i));
+    }
+}
+
+void dump_status()
+{
+    byte b = SerialDBG.read();
+    switch (b)
+    {
+    case 'P':
+        dumpParameters();
+        break;
+    case 'G':
+        dumpGroupAddresses();
+        break;
+} 
 }
 /**
  * @brief HiJack the Arduino setup() function
@@ -110,6 +164,8 @@ void loop()
     knx.loop();
     
     if( DUE(httpHandle)) httpServer.handleClient();
+
+    if (Serial.available()) dump_status();
 
     if(!knx.configured() || knx.progMode()) return;
 
