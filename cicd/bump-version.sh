fb95ef86c599a9e2ea92b7a3c4cd9c0a598de6ee
Erik Meinders <erik@easytocloud.com>
2024-08-08 21:16:14 +0900

    feat: added version bumper

--- cicd/bump-version.sh
+++ cicd/bump-version.sh
@@ -1,6 +1,8 @@
 v=$(git tag | cut -c2- )
-echo Tag vesion is $v
+echo Tag vesion is v$v
 
 jq --arg new_version "$v" '.version = $new_version' library.json > tmp.$$.json && mv tmp.$$.json library.json
 
+# Display version key of library.json
 
+echo Library version $(jq .version library.json)
