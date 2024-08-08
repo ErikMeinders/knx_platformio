2d7b7d25f3121ea73c916f10df80e1c92c8d1000
Erik Meinders <erik@easytocloud.com>
2024-08-08 21:35:05 +0900

    fix: changed version in library to read will be replaced

--- cicd/bump-version.sh
+++ cicd/bump-version.sh
@@ -1,3 +1,7 @@
+#
+# change version in library.json to be in sync with the tag on the repo
+# this version of the library file is not pushed!
+
 v=$(git tag |tail -1| cut -c2- )
 echo Tag vesion is v$v
 
